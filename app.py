d8a0aed1b5eaf62df053433abc0f64b499002212
Osho Tripathi <tripathiosho@gmail.com>
2024-10-04 05:20:01 +0530

    Updated Code, minor optimizations

--- app.py
+++ app.py
@@ -1,60 +1,69 @@
 import pickle
 import streamlit as st
 import requests
+import numpy as np
+from scipy.sparse import csr_matrix
 
+# Fetch poster based on movie_id
 def fetch_poster(movie_id):
-    url = "https://api.themoviedb.org/3/movie/{}?api_key=8265bd1679663a7ea12ac168da84d2e8&language=en-US".format(movie_id)
-    data = requests.get(url)
-    data = data.json()
-    poster_path = data['poster_path']
-    full_path = "https://image.tmdb.org/t/p/w500/" + poster_path
-    return full_path
+    try:
+        url = f"https://api.themoviedb.org/3/movie/{movie_id}?api_key=8265bd1679663a7ea12ac168da84d2e8&language=en-US"
+        data = requests.get(url).json()
+        poster_path = data.get('poster_path', '')
+        if poster_path:
+            full_path = "https://image.tmdb.org/t/p/w500/" + poster_path
+            return full_path
+        return ""
+    except Exception as e:
+        st.error(f"Error fetching poster: {str(e)}")
+        return ""
 
+# Lazy load similarity row only
 def recommend(movie):
-    index = movies[movies['title'] == movie].index[0]
-    distances = sorted(list(enumerate(similarity[index])), reverse=True, key=lambda x: x[1])
-    recommended_movie_names = []
-    recommended_movie_posters = []
-    for i in distances[1:6]:
-        # fetch the movie poster
-        movie_id = movies.iloc[i[0]].movie_id
-        recommended_movie_posters.append(fetch_poster(movie_id))
-        recommended_movie_names.append(movies.iloc[i[0]].title)
+    try:
+        index = movies[movies['title'] == movie].index[0]
+        similarity_row = similarity[index].toarray().flatten()  # Get dense row from sparse matrix
+        distances = sorted(list(enumerate(similarity_row)), reverse=True, key=lambda x: x[1])
+        recommended_movie_names = []
+        recommended_movie_posters = []
 
-    return recommended_movie_names,recommended_movie_posters
+        for i in distances[1:6]:  # Fetch top 5 movies excluding the selected one
+            movie_id = movies.iloc[i[0]].movie_id
+            recommended_movie_posters.append(fetch_poster(movie_id))
+            recommended_movie_names.append(movies.iloc[i[0]].title)
 
+        return recommended_movie_names, recommended_movie_posters
+    except IndexError:
+        st.error("Movie not found in the list!")
+        return [], []
+    except Exception as e:
+        st.error(f"An error occurred: {str(e)}")
+        return [], []
 
+# Streamlit UI
 st.header('Movie Recommender System')
-movies = pickle.load(open('movie_list.pkl','rb'))
-similarity = pickle.load(open('similarity.pkl','rb'))
 
+# Load movies data
+movies = pickle.load(open('movie_list.pkl', 'rb'))
+
+# Load the similarity matrix as sparse for memory efficiency
+similarity_dense = pickle.load(open('similarity.pkl', 'rb'))
+similarity = csr_matrix(similarity_dense)  # Convert to sparse matrix
+
+# Select movie
 movie_list = movies['title'].values
 selected_movie = st.selectbox(
     "Type or select a movie from the dropdown",
     movie_list
 )
 
+# Show recommendation when button is pressed
 if st.button('Show Recommendation'):
-    recommended_movie_names,recommended_movie_posters = recommend(selected_movie)
-    col1, col2, col3, col4, col5 = st.columns(5)
-    with col1:
-        st.text(recommended_movie_names[0])
-        st.image(recommended_movie_posters[0])
-    with col2:
-        st.text(recommended_movie_names[1])
-        st.image(recommended_movie_posters[1])
-
-    with col3:
-        st.text(recommended_movie_names[2])
-        st.image(recommended_movie_posters[2])
-    with col4:
-        st.text(recommended_movie_names[3])
-        st.image(recommended_movie_posters[3])
-    with col5:
-        st.text(recommended_movie_names[4])
-        st.image(recommended_movie_posters[4])
-
-
-
-
-
+    recommended_movie_names, recommended_movie_posters = recommend(selected_movie)
+    
+    if recommended_movie_names:
+        cols = st.columns(len(recommended_movie_names))
+        for idx, col in enumerate(cols):
+            with col:
+                st.text(recommended_movie_names[idx])
+                st.image(recommended_movie_posters[idx])
